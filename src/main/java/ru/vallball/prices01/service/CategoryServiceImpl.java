2c98c9297e0638fa03e9953dd71bab22c3582e19
Valentin <vall-ball@yandex.ru>
2025-02-07 15:45:38 +0700

    Com

--- src/main/java/ru/vallball/prices01/service/CategoryServiceImpl.java
+++ src/main/java/ru/vallball/prices01/service/CategoryServiceImpl.java
@@ -1,5 +1,50 @@
 package ru.vallball.prices01.service;
 
-public class CategoryServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ru.vallball.prices01.model.Category;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService {
+
+	@Override
+	public void save(Category category) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Category category) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Category findCategoryById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Category findCategoryByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
